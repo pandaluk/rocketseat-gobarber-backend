0935524fe43a2345d247bc9fa4a1300a7c989805
Arthur Santos <arthur.silva1@hotmail.com.br>
2019-07-14 09:03:36 -0300

    feature

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '8df27399afe919',
+    pass: '98f8988cd5702f',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreplay@gobarber.com>',
+  },
+};
